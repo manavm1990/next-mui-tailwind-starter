88a326fd72f54a61c24775b80f813ed2a614dda2
Manav Misra <manavm@visionwebsoft.com>
2023-05-08 09:03:44 -0500

    chore: :wrench: add alias for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
+  resolve: {
+    alias: {
+      "@": "/src",
+    },
   },
-})
+  test: {
+    environment: "jsdom",
+  },
+});
